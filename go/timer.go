a5867f9342514e892d02f2f694982e85afe20107
mikebochenek <mike.bochenek@gmail.com>
2013-08-30 08:53:02 +0200

    making it clearer with comments and printf

--- go/timer.go
+++ go/timer.go
@@ -6,20 +6,23 @@ import (
 	"time"
 )
 
+/**
+ * Three ways to use a Timer from http://golang-examples.tumblr.com/
+ */
 func main() {
 	// (A)
 	time.AfterFunc(5*time.Second, func() {
-		fmt.Printf("expired")
+		fmt.Printf("(A) expired \n")
 	})
 
 	// (B) create a Timer object
 	timer := time.NewTimer(5 * time.Second)
 	<-timer.C
-	fmt.Printf("expired")
+	fmt.Printf("(B) expired \n")
 
 	// (C) time.After() returns timer.C internally
 	<-time.After(5 * time.Second)
-	fmt.Printf("expired")
+	fmt.Printf("(C) expired \n")
 
 	fmt.Println("Hello World!")
 }

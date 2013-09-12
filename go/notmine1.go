5255f88894801c60eeff1d506140d6923f614caf
mikebochenek <mike.bochenek@gmail.com>
2013-09-12 22:06:33 +0200

    copied from http://play.golang.org/p/eGRSgZqJyQ and it looks fun

---
+++ go/notmine1.go
@@ -0,0 +1,22 @@
+// notmine1
+// http://play.golang.org/p/eGRSgZqJyQ
+package main
+
+type I interface {
+	Set(int)
+}
+
+type T struct {
+	Value int
+}
+
+func (t *T) Set(v int) {
+	t.Value = v
+}
+
+func main() {
+	t := &T{}
+	var i I = t
+	i.Set(23)
+	println(t.Value)
+}

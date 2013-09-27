d724f6a34f3bea04feb233d3494eb6c12995ad3f
mikebochenek <mike.bochenek@gmail.com>
2013-09-27 23:04:56 +0200

    playing with time etc.

--- go/hello.go
+++ go/hello.go
@@ -1,9 +1,15 @@
-// +build hello
+/* +build hello */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	now := time.Now()
+	fmt.Printf("hello, world with fmt %d\n", now)
+	log.Printf("hello, world with log %d\n", now)
 }

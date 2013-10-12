01faa0a6804c3b3c998ff07fb28f0838d4fbf550
mikebochenek <mike.bochenek@gmail.com>
2013-10-12 23:17:29 +0200

    adding little script for checking all git repos

---
+++ linux/github-status-all.sh
@@ -0,0 +1,45 @@
+clear
+
+cd ~/Dev/workspace/brainstorm
+echo 
+echo ~/Dev/workspace/brainstorm
+git status
+
+cd ~/Dev/workspace/cvparse
+echo 
+echo ~/Dev/workspace/cvparse
+git status
+
+cd ~/Dev/workspace/zoo
+echo 
+echo ~/Dev/workspace/zoo
+git status
+
+cd ~/Dev/workspace/zoo-mint
+echo 
+echo ~/Dev/workspace/zoo-mint
+git status
+
+cd ~/Dev/learning
+echo 
+echo ~/Dev/learning
+git status
+
+cd ~/Dev/vantaa4
+echo 
+echo ~/Dev/vantaa4
+git status
+
+cd ~/Dev/gocode/src/myapp0
+echo 
+echo ~/Dev/gocode/src/myapp0
+git status
+
+cd ~/Dev/gocode/src/mychat
+echo 
+echo ~/Dev/gocode/src/mychat
+git status
+
+
+echo should also add this at some point:  ~/Dev/adt-workspace
+echo should also add this at some point:  college

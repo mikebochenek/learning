262f193859b9d56e3578c52c7a72e77d13f5c536
mikebochenek <mike.bochenek@gmail.com>
2013-10-21 22:56:01 +0200

    finished backup script

--- linux/backup-github.sh
+++ linux/backup-github.sh
@@ -7,4 +7,19 @@ cd /tmp
 MYNOW=$(date +"%F-%H.%M.%S")
 mkdir backup-$MYNOW
 cd backup-$MYNOW
+
 git clone https://github.com/mikebochenek/learning
+git clone https://github.com/mikebochenek/brainstorm
+git clone https://github.com/mikebochenek/zoo-mint
+git clone https://github.com/mikebochenek/adt-workspace
+git clone https://github.com/mikebochenek/myapp0
+git clone https://github.com/mikebochenek/zoo
+git clone https://github.com/mikebochenek/vantaa
+git clone https://github.com/mikebochenek/mychat
+git clone https://github.com/mikebochenek/college
+git clone https://github.com/mikebochenek/cvparse
+
+cd /tmp
+tar cvf backup-$MYNOW.tar /tmp/backup-$MYNOW/*
+gzip backup-$MYNOW.tar
+

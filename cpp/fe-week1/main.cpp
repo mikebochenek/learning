c2c676377c3ca1bfd7fc9d61dc962824735af998
mikebochenek <mike.bochenek@gmail.com>
2013-11-02 21:29:36 +0100

    first week main cpp

---
+++ cpp/fe-week1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double total = 0.0;
+    for (int i = 0; i < 20; i++) {
+        total += 50000 / pow (1.1, i);
+        //cout << total << endl;
+        printf ("%f\n", total);
+    }
+
+    return 0;
+}

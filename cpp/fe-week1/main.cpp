bf5e5ab39eee267a26f940ca72539c222aa9f392
mikebochenek <mike.bochenek@gmail.com>
2013-11-09 23:10:16 +0100

    refactor question1 and start playing with algorithms

--- cpp/fe-week1/main.cpp
+++ cpp/fe-week1/main.cpp
@@ -4,14 +4,24 @@
 
 using namespace std;
 
-int main()
-{
+void question1() {
     double total = 0.0;
     for (int i = 0; i < 20; i++) {
         total += 50000 / pow (1.1, i);
         //cout << total << endl;
-        printf ("%f\n", total);
+        //printf ("%f\n", total);
     }
+}
 
+int fibonacci(int i) {
+    if (i <= 1) { return 1; }
+    else { return fibonacci(i - 2) + fibonacci(i - 1); }
+}
+
+int main() {
+    question1();
+    for (int i = 0; i < 10; i++) {
+        cout << fibonacci(i) << endl;
+    }
     return 0;
 }

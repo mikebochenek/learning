04fa9cd54cdec521b3a457e36dc8328369a8eff6
mikebochenek <mike.bochenek@gmail.com>
2013-11-09 23:26:21 +0100

    playing around with time..

--- cpp/fe-week1/main.cpp
+++ cpp/fe-week1/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 
 using namespace std;
+
+int timeexample();
 
 void question1() {
     double total = 0.0;
@@ -29,6 +30,27 @@ int main() {
         //cout << fibonacci(i) << endl;
         fibonacci(i);
     }
+    timeexample();
     return 0;
 }
 
+int timeexample() {
+   struct timespec tim, tim2;
+   tim.tv_sec  = 0;
+   tim.tv_nsec = 100000000L;
+   int i = 0;
+
+   for (i = 0; i < 10; i++) {
+      if(nanosleep(&tim , &tim2) < 0 ) {
+         printf("Nano sleep system call failed \n");
+         return -1;
+      } else {
+		  printf("%10d.%d\n", i / 10, i % 10);
+		  fflush(stdout);
+	  }
+   }
+
+   printf("Nano sleep successfull \n");
+
+   return 0;
+}

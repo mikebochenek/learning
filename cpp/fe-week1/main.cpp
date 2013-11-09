618d13f577a6b7a050a6d11ac5bb8871d434b838
mikebochenek <mike.bochenek@gmail.com>
2013-11-09 23:16:23 +0100

    adding other libraries and code reformat

--- cpp/fe-week1/main.cpp
+++ cpp/fe-week1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 using namespace std;
 
@@ -14,14 +16,19 @@ void question1() {
 }
 
 int fibonacci(int i) {
-    if (i <= 1) { return 1; }
-    else { return fibonacci(i - 2) + fibonacci(i - 1); }
+    if (i <= 1) {
+        return 1;
+    } else {
+        return fibonacci(i - 2) + fibonacci(i - 1);
+    }
 }
 
 int main() {
     question1();
     for (int i = 0; i < 10; i++) {
-        cout << fibonacci(i) << endl;
+        //cout << fibonacci(i) << endl;
+        fibonacci(i);
     }
     return 0;
 }
+

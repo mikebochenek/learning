43e5b4ff9b59c6f7dd2f5bf68e62e5791dbd0e28
mikebochenek <mike.bochenek@gmail.com>
2014-02-03 22:42:53 +0100

    feb 3 reads and more sqls

--- sql/queries-02.sql
+++ sql/queries-02.sql
@@ -1,4 +1,6 @@
 select * from bochenekblog.Member;
 
 select * from bochenekblog.b2posts;
+select * from bochenekblog.b2posts where post_date between '2004-01-01' and '2004-12-31';
+
 select * from bochenekblog.b2comments;

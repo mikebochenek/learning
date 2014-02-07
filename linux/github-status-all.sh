515fae2cf8a1467eff826e99329e0959cdc20bc0
mikebochenek <mike.bochenek@gmail.com>
2014-02-07 23:04:09 +0100

    put everything on one line and use -s option

--- linux/github-status-all.sh
+++ linux/github-status-all.sh
@@ -1,45 +1,17 @@
 clear
 
-cd ~/Dev/workspace/brainstorm
-git status
-
-cd ~/Dev/workspace/cvparse
-git status
-
-cd ~/Dev/workspace/zoo
-git status
-
-cd ~/Dev/workspace/zoo-mint
-git status
-
-cd ~/Dev/learning
-git status
-
-cd ~/Dev/vantaa4
-git status
-
-cd ~/Dev/gocode/src/myapp0
-git status
-
-cd ~/Dev/gocode/src/mychat
-git status
-
-cd ~/Dev/workspace/idone
-git status
-
-cd ~/Dev/workspace/angular-jboss-blog
-git status
-
-cd ~/Dev/AptanaWorkspace/html5
-git status
-
-cd ~/Dev/college
-git status
-
-cd ~/Dev/adt-workspace
-git status
-
-cd ~/Dev/adt-workspace3
-git status
-
+cd ~/Dev/workspace/brainstorm && pwd && git status -s
+cd ~/Dev/workspace/cvparse && pwd && git status -s
+cd ~/Dev/workspace/zoo && pwd && git status -s
+cd ~/Dev/workspace/zoo-mint && pwd && git status -s
+cd ~/Dev/learning && pwd && git status -s
+cd ~/Dev/vantaa4 && pwd && git status -s
+cd ~/Dev/gocode/src/myapp0 && pwd && git status -s 
+cd ~/Dev/gocode/src/mychat && pwd && git status -s
+cd ~/Dev/workspace/idone && pwd && git status -s
+cd ~/Dev/workspace/angular-jboss-blog && pwd && git status -s
+cd ~/Dev/AptanaWorkspace/html5 && pwd && git status -s
+cd ~/Dev/college && pwd && git status -s
+cd ~/Dev/adt-workspace && pwd && git status -s
+cd ~/Dev/adt-workspace3 && pwd && git status -s
 

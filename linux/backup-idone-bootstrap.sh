2b794a63def6d74b476de050c89a7b29bb68806c
mikebochenek <mike.bochenek@gmail.com>
2014-03-01 21:32:58 +0100

    update backup script

--- linux/backup-idone-bootstrap.sh
+++ linux/backup-idone-bootstrap.sh
@@ -6,6 +6,7 @@ mkdir backup-idoneweb-$MYNOW
 cd backup-idoneweb-$MYNOW
 
 cp -r /home/mike/Dev/AptanaWorkspace/idone-bootstrap/ .
+cp -r /home/mike/Dev/AptanaWorkspace/bochenek-ch/ .
 
 cd /tmp
 tar cvf backup-idoneweb-$MYNOW.tar /tmp/backup-idoneweb-$MYNOW/*

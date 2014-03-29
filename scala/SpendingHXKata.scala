6fc55c7597777352fdbe7b8856a6ddcde3affd19
mikebochenek <mike.bochenek@gmail.com>
2014-03-29 23:11:16 +0100

    still workin' on it...

--- scala/SpendingHXKata.scala
+++ scala/SpendingHXKata.scala
@@ -1,15 +1,24 @@
 package zoo
 
+import scala.collection.mutable.MutableList
+
 object SpendingHXKata {
 
   def main(args: Array[String]): Unit = {
     val catalog = Map(5 -> "stapler", 3 -> "pen", 2 -> "pencil", 1 -> "paper")
-    spend(catalog, 10)
+    val basket: MutableList[String] = MutableList()
+
+    spend(catalog, 5, basket)
   }
   
-  def spend(c: Map[Int, String], amount: Int) {
-    val basket: List[String] = List()
-    c.keys.foreach(i => if (amount == i) { basket+c(i) }  )
+  def spend(c: Map[Int, String], amount: Int, basket: MutableList[String]) {
+    
+    c.keys.foreach(i => if (amount <= i && amount > 0) {
+      basket += c(i)
+      spend(c, amount - i, basket)
+    })
+    
+    if (!basket.isEmpty && amount == 0) println(basket)
   }
 
 }

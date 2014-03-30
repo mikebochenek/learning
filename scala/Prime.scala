525d4babf622e82459ba66b5dc63842270bf20fa
mikebochenek <mike.bochenek@gmail.com>
2014-03-30 13:49:54 +0200

    migrated Prime example from java, could be another good example - but still needs cleanup and optimizations

---
+++ scala/Prime.scala
@@ -0,0 +1,59 @@
+package zoo
+
+import org.junit.Assert._
+import org.junit.Test
+
+/**
+ * page 164 of Discrete Mathematics and Its Applications by Kenneth H. Rosen
+ * question #6. Given a positive integer, determine whether it is prime.
+ *
+ * @author Mike Bochenek
+ */
+class Prime {
+
+  /**
+   * It'd be fun to more optimizations here
+   * for example, we could stop at sqrt(x) and keep a map of primes
+   */
+  def isPrime(x: Int): Boolean = {
+    val sqrtx = x //TODO optimize: Math.sqrt(x)
+    for (i <- 2 until sqrtx.toInt by 2) {
+      if (sqrtx.toInt % i == 0) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  @Test def test4() { assertFalse(isPrime(4)); }
+
+  @Test def test7() { assertTrue(isPrime(7)); }
+
+  @Test def test23244() { assertFalse(isPrime(23244)); } // 23244 = 2 x 2 x 3 x 13 x 149 
+
+  /**
+   * http://en.wikipedia.org/wiki/Prime_number#Definition_and_examples
+   */
+  @Test
+  def testFromWikipedia() {
+    val primes = Array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+      43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107,
+      109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+      179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239,
+      241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
+      313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383,
+      389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457,
+      461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
+      547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613,
+      617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683,
+      691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769,
+      773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857,
+      859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
+      947, 953, 967, 971, 977, 983, 991, 997);
+
+    for (p <- primes) {
+      assertTrue("" + p + " is in fact prime.", isPrime(p));
+    }
+  }
+}

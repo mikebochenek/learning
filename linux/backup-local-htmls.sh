5302c5337b8bd577ef71e6ee3b0ab01d5ae08e2e
mikebochenek <mike.bochenek@gmail.com>
2014-04-11 23:06:16 +0200

    add two more local htmls for backup

--- linux/backup-local-htmls.sh
+++ linux/backup-local-htmls.sh
@@ -8,6 +8,8 @@ cd backup-idoneweb-$MYNOW
 cp -r /home/mike/Dev/AptanaWorkspace/idone-bootstrap/ .
 cp -r /home/mike/Dev/AptanaWorkspace/idone-coming/ .
 cp -r /home/mike/Dev/AptanaWorkspace/bochenek-ch/ .
+cp -r /home/mike/Dev/AptanaWorkspace/fitness/ .
+cp -r /home/mike/Dev/AptanaWorkspace/play/ .
 
 cd /tmp
 tar cvf backup-idoneweb-$MYNOW.tar /tmp/backup-idoneweb-$MYNOW/*

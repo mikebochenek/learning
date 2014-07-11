bf5ea28f206dfd168669f2c5adbcd65f9f6cf8e5
mikebochenek <mike.bochenek@gmail.com>
2014-07-11 22:03:59 +0200

    losing ruby virginity

---
+++ ruby/say_hi.rb
@@ -0,0 +1,23 @@
+class HelloWorld
+  def doHello()
+    puts "Hello World"
+  end
+
+  # Say hi to everybody
+  def say_hi
+    if @names.nil?
+      puts "..."
+    elsif @names.respond_to?("each")
+      # @names is a list of some kind, iterate!
+      @names.each do |name|
+        puts "Hello #{name}!"
+      end
+    else
+      puts "Hello #{@names}!"
+    end
+  end
+
+end
+
+HelloWorld.new().doHello()
+HelloWorld.new().say_hi()  

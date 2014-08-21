b67669c445d219d915736b0a777b6d572bbd7456
mikebochenek <mike.bochenek@gmail.com>
2014-08-21 23:12:36 +0200

    more updates... are we actually done in only 23:11:04 up  2:29,  1 user,  load average: 0.78, 0.81, 0.74

--- ruby/zillow-play1.rb
+++ ruby/zillow-play1.rb
@@ -5,16 +5,53 @@
 # http://stackoverflow.com/questions/5795309/gem-install-mysql-fail
 # http://zetcode.com/db/mysqlrubytutorial/
 # http://rubylearning.com/satishtalim/ruby_mysql_tutorial.html
+# http://ruby.bastardsbook.com/chapters/sql/
 
 require "mysql"
 require "mysql2"
+
+
+def calc_score(city, year, m)
+
+  sql1 = "SELECT sum(num_taxpayers) as total
+    FROM test.incomes 
+    where municipality like '" + city + "' and year = " + year + ";"
+  rs = m.query(sql1)
+  total_taxpayers = rs.fetch_row[0]
+
+  # puts city + " " + year + " total_taxpayers: %.2f" % total_taxpayers.to_i
+
+
+  sql2 = "SELECT income_group, sum(num_taxpayers) as total_count
+    FROM test.incomes 
+    where municipality like '" + city + "' and year = " + year + "
+    group by income_group
+    order by income_group asc;"
+  rs = m.query(sql2)
+  total_score = 0
+  rs.each_hash do |row|
+    # puts row['income_group'] + '  ' + row['total_count']
+    tc = row['total_count']
+    ig = row['income_group'] 
+    total_score += 1.0 * tc.to_i * ig.to_i / total_taxpayers.to_i
+  end
+  puts city + " " + year + " total_score: %.2f" % total_score.to_s + "  [" + total_taxpayers + "]"
+
+  total_score
+end
+
+# calc_score('Zollikon', '2010', m)
+# calc_score('Z%rich', '2010', m)
+# calc_score('K%snacht', '2010', m)
+# calc_score('Altikon', '2010', m)
+
 
 
 sql = "select municipality, count(*) as cc
   FROM test.incomes 
   WHERE municipality <> 'MUNICIPALITY'
   group by municipality
-  order by cc desc;"
+  order by municipality asc;"
 
 m = Mysql.new("localhost", "test", "")
 m.select_db('test')
@@ -27,7 +64,9 @@ rs = m.query(sql)
     
 rs.each_hash do |row|
   # puts row.join("\s") # print on one line
-  puts row['municipality']
+  # puts row['municipality']
+  mun = row['municipality']
+  calc_score(mun, '2010', m)
 end
 
 
@@ -37,5 +76,66 @@ client = Mysql2::Client.new(
   :username => 'test')
 res = client.query(sql, :as => :json)
 res.each do |row|
-  puts row
+  # puts row
 end 
+
+
+thoughts = "here is what I am thinking, although I should probably think this through even more
+create a .json file for each year
+each file will contain a tree, with municipality as root
+followed by additional information such as score etc. and individual values
+home page could return a static .png file, until user clicks on it (and only then load interactive one)"
+
+
+
+# so how do we calculate score?
+# municipality with highest percentage of 45, 44, etc.
+# Zollikon total is 7294, Zurich 233832
+
+income_group_descriptions = "INCOME GROUP CODE;INCOME GROUP DESCRIPTION (CHF)
+1;0-9999
+2;10000-19999
+3;20000-29999
+4;30000-39999
+5;40000-49999
+6;50000-59999
+7;60000-69999
+8;70000-79999
+9;80000-89999
+10;90000-99999
+11;100000-109999
+12;110000-119999
+13;120000-129999
+14;130000-139999
+15;140000-149999
+16;150000-159999
+17;160000-169999
+18;170000-179999
+19;180000-189999
+20;190000-199999
+21;200000-209999
+22;210000-219999
+23;220000-229999
+24;230000-239999
+25;240000-249999
+26;250000-259999
+27;260000-269999
+28;270000-279999
+29;280000-289999
+30;290000-299999
+31;300000-349999
+32;350000-399999
+33;400000-449999
+34;450000-499999
+35;500000-549999
+36;550000-599999
+37;600000-649999
+38;650000-699999
+39;700000-749999
+40;750000-799999
+41;800000-849999
+42;850000-899999
+43;900000-949999
+44;950000-999999
+45;1000000+
+"

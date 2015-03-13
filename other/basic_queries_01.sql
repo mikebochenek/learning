151ed865b3ec64ed8a7785b98179cdbfac89412c
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2015-03-13 21:40:46 +0100

    basic sqls for arvados db

---
+++ other/basic_queries_01.sql
@@ -0,0 +1,11 @@
+﻿select * from users;
+update users set is_admin = true where id = 3;
+update users set is_active = true where id = 4;
+
+commit;
+
+
+select * from groups;
+select * from traits;
+
+select owner_uuid from api_clients;

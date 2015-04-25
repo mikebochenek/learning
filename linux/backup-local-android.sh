2e0552d94d361d0c25cdb7938e9e137c16a5f8de
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2015-04-25 22:42:14 +0200

    update android backup script

--- linux/backup-local-android.sh
+++ linux/backup-local-android.sh
@@ -5,12 +5,16 @@ MYNOW=$(date +"%F-%H.%M.%S")
 mkdir backup-android-$MYNOW
 cd backup-android-$MYNOW
 
-cp -r /home/mike/Dev/adt-workspace4/fitness/ .
-cp -r /home/mike/Dev/adt-workspace4/TouchMemoryCH/ .
+# old HP laptop
+# cp -r /home/mike/Dev/adt-workspace4/fitness/ .
+# cp -r /home/mike/Dev/adt-workspace4/TouchMemoryCH/ .
+
+# new HP laptop
+cp -r /home/mike/AndroidstudioProjects/TouchMemoryCH/ .
 
 cd /tmp
-tar cvf backup-android-$MYNOW.tar /tmp/backup-android-$MYNOW/*
-gzip backup-android-$MYNOW.tar
+tar cvf backup-NEW-android-$MYNOW.tar /tmp/backup-android-$MYNOW/*
+gzip backup-NEW-android-$MYNOW.tar
 cp backup*.gz ~/Dropbox/backups/
-mv /tmp/backup-android-$MYNOW.tar.gz /tmp/current-version-android.tar.gz
+mv /tmp/backup-NEW-android-$MYNOW.tar.gz /tmp/current-version-android.tar.gz
 # mv /tmp/current-version-android.tar.gz ~/Dropbox/fitness_app/

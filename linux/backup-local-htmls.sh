d154e8672989c685774e4aea0003bbadf9ae579e
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2015-04-25 22:46:30 +0200

    backup local htmls script

--- linux/backup-local-htmls.sh
+++ linux/backup-local-htmls.sh
@@ -5,14 +5,23 @@ MYNOW=$(date +"%F-%H.%M.%S")
 mkdir backup-idoneweb-$MYNOW
 cd backup-idoneweb-$MYNOW
 
-cp -r /home/mike/Dev/AptanaWorkspace/idone-bootstrap/ .
-cp -r /home/mike/Dev/AptanaWorkspace/idone-coming/ .
-cp -r /home/mike/Dev/AptanaWorkspace/bochenek-ch/ .
-cp -r /home/mike/Dev/AptanaWorkspace/fitness/ .
-cp -r /home/mike/Dev/AptanaWorkspace/play/ .
-cp -r /home/mike/Dev/AptanaWorkspace/maps/ .
+# old HP laptop
+# cp -r /home/mike/Dev/AptanaWorkspace/idone-bootstrap/ .
+# cp -r /home/mike/Dev/AptanaWorkspace/idone-coming/ .
+# cp -r /home/mike/Dev/AptanaWorkspace/bochenek-ch/ .
+# cp -r /home/mike/Dev/AptanaWorkspace/fitness/ .
+# cp -r /home/mike/Dev/AptanaWorkspace/play/ .
+# cp -r /home/mike/Dev/AptanaWorkspace/maps/ .
+
+# new HP laptop
+cp -r /home/mike/Documents/static-html/idone-bootstrap/ .
+cp -r /home/mike/Documents/static-html/idone-coming/ .
+cp -r /home/mike/Documents/static-html/bochenek-ch/ .
+cp -r /home/mike/Documents/static-html/fitness/ .
+cp -r /home/mike/Documents/static-html/play/ .
+cp -r /home/mike/Documents/static-html/maps/ .
 
 cd /tmp
-tar cvf backup-idoneweb-$MYNOW.tar /tmp/backup-idoneweb-$MYNOW/*
-gzip backup-idoneweb-$MYNOW.tar
+tar cvf backup-NEW-idoneweb-$MYNOW.tar /tmp/backup-idoneweb-$MYNOW/*
+gzip backup-NEW-idoneweb-$MYNOW.tar
 cp backup*.gz ~/Dropbox/backups/

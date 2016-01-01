725c1caac315aea62f4b20977ae50be6305ad401
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-01-01 21:51:25 +0100

    update alias quick start for presto and backup script for presto backend code

---
+++ linux/backup-presto.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /tmp
+MYNOW=$(date +"%F-%H.%M.%S")
+mkdir backup-presto-$MYNOW
+cd backup-presto-$MYNOW
+
+cp -r /home/mike/Documents/prestop/ .
+
+cd /tmp
+tar cvf backup-presto-$MYNOW.tar /tmp/backup-presto-$MYNOW/*
+gzip backup-presto-$MYNOW.tar
+cp backup-presto*.gz ~/Dropbox/backups/
+cp backup-presto*.gz ~/Dropbox/presto/backup/backend-code/
+
+

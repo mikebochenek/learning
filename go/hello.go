6e202326b0cd3faf9c7496b1ff70a59d18dc02ce
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-10-08 23:01:55 +0200

    trying go iris

--- go/hello.go
+++ go/hello.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func main() {
+func main_old() {
 	now := time.Now()
 	fmt.Printf("\nhello, world with fmt [ %v ]\n", now)
 	log.Printf("\nhello, world with log [ %v ]\n", now)

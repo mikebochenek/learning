176977c24fdd3040fb729c087a4f838b600949e0
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2016-10-13 23:39:47 +0200

    playing around with concurrency

---
+++ go/concurrency/goroutines.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+// https://tour.golang.org/methods/9
+type Abser interface {
+	Abs() float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func tryinterfaces() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // a MyFloat implements Abser
+	a = &v // a *Vertex implements Abser
+
+	// In the following line, v is a Vertex (not *Vertex)
+	// and does NOT implement Abser.
+	//a = v
+
+	fmt.Println(a.Abs())
+}
+
+// https://tour.golang.org/concurrency/2
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	tryinterfaces()
+
+	go say("world")
+	go say("hello")
+	/* go */ saySomethingElse("and another thing...")
+
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+
+}
+
+func saySomethingElse(s string) {
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println(s)
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}

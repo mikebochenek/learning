d4fdabb5ccbe8b8c281e9fd7a17cc1fbf639a769
Mike Bochenek <mike@bochenek.ca>
2019-01-02 23:16:39 +0100

    WIP euler #218 - add GCD function and test for perfectArea(mod 6 and 28)

--- go/katas/project_euler_218_test.go
+++ go/katas/project_euler_218_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-//var UPPER_BOUND = 1000000
+var UPPER_BOUND = int64(1000000) //TODO int64(1000000 * 1000000 * 1000)
 
 /*
  * https://projecteuler.net/problem=218
@@ -13,12 +13,40 @@ import (
  */
 func TestPerfectRightAngledTriangles(t *testing.T) {
 
+	// fmt.Println(gcd(25, 5))
+	// fmt.Println(gcd(625, 150))
+
 	if x := countPRAT(); x != 31 {
 		t.Errorf("we expected 31, but got %v", x)
 	}
 }
 
 func countPRAT() int32 {
+	max := int64(UPPER_BOUND)
+	for i := int64(1); i < max; i += 1 {
+		//TODO
+	}
 
 	return 31
+}
+
+func gcd(x int64, y int64) int64 {
+	max := x
+	if y > x {
+		max = y
+	}
+
+	gcd := int64(1)
+	for i := int64(1); i < max; i += 1 {
+		if y%i == 0 && x%i == 0 {
+			gcd = i
+		}
+	}
+	return gcd
+}
+
+/* its area is a multiple of the perfect numbers 6 and 28. */
+func perfectArea(a int64, b int64) bool {
+	area := a * b
+	return (area%6 == 0 && area%28 == 0)
 }

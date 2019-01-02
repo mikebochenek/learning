41a4d8bbfd2873e51060459ba7b6b242504df9cb
Mike Bochenek <mike@bochenek.ca>
2019-01-02 09:31:03 +0100

    remove outputs in bcrypt, since I will not implement this anytime soon anyways

--- go/katas/bcrypt_test.go
+++ go/katas/bcrypt_test.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"testing"
-	"time"
 )
 
 func bcrypt(x string) string {
@@ -13,7 +10,7 @@ func bcrypt(x string) string {
 
 func TestBCrypt(t *testing.T) {
 	bcrypt("xx")
-	now := time.Now()
-	fmt.Printf("\n  testing with fmt [ %v ]\n", now)
-	log.Printf("\n  testing with log [ %v ]\n", now)
+	// now := time.Now()
+	// fmt.Printf("\n  testing with fmt [ %v ]\n", now)
+	// log.Printf("\n  testing with log [ %v ]\n", now)
 }

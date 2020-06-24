c7246212ab4649d8f7672ace956a793354935fa0
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2020-06-24 21:54:10 +0200

    apache2 sites-enabled/* from docean

--- python3/fast_file_read.py
+++ python3/fast_file_read.py
@@ -1,6 +1,7 @@
 # https://stackabuse.com/read-a-file-line-by-line-in-python/
 
-with open('/Users/mike/Downloads/travel-types-data.csv') as f:
+with open('/home/mike/Downloads-1/cities.csv') as f:
+#with open('/Users/mike/Downloads/travel-types-data.csv') as f:
     for line in f:
         print (line, end=' ')  # Do something with 'line'
         

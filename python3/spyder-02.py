0f68c2c55f648392eae7ad7428782221cda8a670
Mike Bochenek <mike.bochenek@gmail.com>
2022-01-23 10:11:16 +0100

    seaborn graph pure awesomeness...

--- python3/spyder-02.py
+++ python3/spyder-02.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Spyder Editor - This is another temporary script file for playing around in 2022
 """
 
 import matplotlib.pyplot as plt
@@ -14,3 +12,17 @@ print(companies.head(20))
 
 # df.groupby(['Company']).size().plot(kind='pie', subplots=True)
 
+# https://seaborn.pydata.org/introduction.html
+import seaborn as sns
+"""
+tips = sns.load_dataset("tips")
+sns.relplot(
+    data=tips,
+    x="total_bill", y="tip", col="time",
+    hue="smoker", style="smoker", size="size",
+)
+"""
+
+# https://stackoverflow.com/questions/46793448/plotting-series-using-seaborn
+head = companies.head(18)
+sns.barplot(head.values, head.index, orient = 'h')

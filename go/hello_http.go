4fc040ad7edda660632843e8d24443108a1677e0
Mike Bochenek <mike.bochenek@gmail.com>
2022-01-25 10:06:24 +0100

    play around: check if number from http param is prime

--- go/hello_http.go
+++ go/hello_http.go
@@ -4,15 +4,32 @@ import (
   "fmt"
   "log"
   "net/http"
+  "strconv"
 )
 
 /*
  * https://blog.devgenius.io/golangs-most-important-feature-is-invisible-6be9c1e7249b
  * % ab -c 160 -n 100000 -s 2 -k  "http://127.0.0.1:9100/"
-*/
+ * test with: http://localhost:8080/?param1=2
+ */
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(w, "Goodbye, World!")
+    param1 := req.URL.Query().Get("param1")
+    x, err := strconv.Atoi(param1)
+    if err != nil {
+      println("invalid input") // funny that this check is "forced" onto the developer
+    }
+    println("request input", param1)
+    fmt.Fprintln(w, x, "is prime?", prime(x))
   })
   log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func prime(x int) bool {
+	for i := 2; i < x; i += 2 {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
 }

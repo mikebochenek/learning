2160ac2827f448f0383965567f4664e19c44e44e
Mike Bochenek <mike.bochenek@gmail.com>
2022-01-26 21:58:01 +0100

    reformat only

--- go/hello_http.go
+++ go/hello_http.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "strconv"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 /*
@@ -13,16 +13,16 @@ import (
  * test with: http://localhost:8080/?param1=2
  */
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-    param1 := req.URL.Query().Get("param1")
-    x, err := strconv.Atoi(param1)
-    if err != nil {
-      println("invalid input") // funny that this check is "forced" onto the developer
-    }
-    println("request input", param1)
-    fmt.Fprintln(w, x, "is prime?", prime(x))
-  })
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		param1 := req.URL.Query().Get("param1")
+		x, err := strconv.Atoi(param1)
+		if err != nil {
+			println("invalid input") // funny that this check is "forced" onto the developer
+		}
+		println("request input", param1)
+		fmt.Fprintln(w, x, "is prime?", prime(x))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func prime(x int) bool {

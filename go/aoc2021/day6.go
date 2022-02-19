8621796b4f65a65aef25fe49acd183dcae32ec17
Mike Bochenek <mike.bochenek@gmail.com>
2022-02-19 10:17:50 +0100

    still working on part 2 of day6...

--- go/aoc2021/day6.go
+++ go/aoc2021/day6.go
@@ -25,7 +25,9 @@ func main() {
 		for i, number := range numbers {
 			pos[i] = safeParse(number)
 		}
-		simulate(pos, 80)
+		//fmt.Println("test after 18 days?:", 5934 == smartSimulate(pos, 80))
+		fmt.Println("test after 18 days?:", 26 == smartSimulate(pos, 18))
+		fmt.Println("test after 18 days?:", 26 == simulate(pos, 18)) // old simulate destroys input array!
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -33,10 +35,38 @@ func main() {
 	}
 }
 
-func simulate(p []int, days int) {
+func smartSimulate(p []int, days int) int {
+	counts := [10]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	for j := 0; j < len(p); j++ {
+		counts[p[j]] = counts[p[j]] + 1
+	}
+	total := sumup(counts)
+	fmt.Println("Initial state:", p, "\t*len:", total, "\t", counts)
 
+	for i := 0; i < days; i++ {
+		counts[9] = counts[0]             // new fish
+		counts[6] = counts[6] + counts[0] // respawn parent
+		for j := 0; j < 9; j++ {
+			counts[j] = counts[j+1]
+		}
+
+		total = sumup(counts)
+		fmt.Println("after day", (i + 1), "\t\t\t len:", total, "\t", counts)
+	}
+	fmt.Println("after", days, "there are", total)
+	return total
+}
+
+func sumup(p [10]int) int {
+	total := 0
+	for j := 0; j < len(p); j++ {
+		total = total + p[j]
+	}
+	return total
+}
+
+func simulate(p []int, days int) int {
 	fmt.Println("Initial state:", p, "\tlen:", len(p))
-
 	for i := 0; i < days; i++ {
 		for j := 0; j < len(p); j++ {
 			if p[j] == 0 {
@@ -46,10 +76,10 @@ func simulate(p []int, days int) {
 				p[j] = p[j] - 1
 			}
 		}
-		//fmt.Println("after day", (i+1), p, "\tlen:", len(p))
+		fmt.Println("after day", (i + 1), p, "\tlen:", len(p))
 	}
-
 	fmt.Println("after", days, "there are", len(p))
+	return len(p)
 }
 
 func safeParse(g string) int {

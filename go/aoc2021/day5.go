210fb185cb8d7062455c862c693f1fdb246997ac
Mike Bochenek <mike.bochenek@gmail.com>
2022-02-21 22:35:26 +0100

    day5 - part 1 of aoc

---
+++ go/aoc2021/day5.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("/home/mike/Documents/day5-0.txt") // open file
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close() // remember to close the file at the end of the program
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var lines = 0
+	const size = 1000
+	var p1 [size][]string
+	var p2 [size][]string
+	for scanner.Scan() {
+		line := scanner.Text()
+		points := strings.Split(line, " -> ")
+		p1[lines] = strings.Split(points[0], ",")
+		p2[lines] = strings.Split(points[1], ",")
+		lines = lines + 1
+		//fmt.Println(lines, p1, p2)
+	}
+
+	var matrix [size][size]int
+
+	for i := 0; i < size; i++ {
+		if p1[i] != nil && p2[i] != nil {
+			fmt.Println(p1[i], p2[i])
+			p1x := safeParse(p1[i][0])
+			p1y := safeParse(p1[i][1])
+			p2x := safeParse(p2[i][0])
+			p2y := safeParse(p2[i][1])
+
+			if p1x == p2x { // vertical
+				startY := p1y
+				endY := p2y
+				if p2y < p1y {
+					startY = p2y
+					endY = p1y
+				}
+				//fmt.Println("vertical at x", p1x, startY, endY)
+				for j := startY; j <= endY; j++ {
+					matrix[j][p1x] = matrix[j][p1x] + 1
+				}
+			}
+			if p1y == p2y { // horizontal
+				startX := p1x
+				endX := p2x
+				if p2x < p1x {
+					startX = p2x
+					endX = p1x
+				}
+				//fmt.Println("horizontal at y", p1y, startX, endX)
+				for j := startX; j <= endX; j++ {
+					matrix[p1y][j] = matrix[p1y][j] + 1
+				}
+			}
+		}
+	}
+
+	count := 0
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if matrix[i][j] > 1 { //== maxOverlap) {
+				count = count + 1
+			}
+		}
+	}
+
+	fmt.Println("count:", count, "maxOverlap:", 2)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func safeParse(g string) int {
+	if i, err := strconv.Atoi(g); err != nil {
+		fmt.Println(err)
+		return -1
+	} else {
+		return i
+	}
+}

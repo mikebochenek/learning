5338b24e91e3cf3945cf4cfac27719df2397747b
Mike Bochenek <mike.bochenek@gmail.com>
2022-03-02 07:48:52 +0100

    going back to day 4 of aoc 2021

--- go/aoc2021/day4.go
+++ go/aoc2021/day4.go
@@ -5,23 +5,27 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	f, err := os.Open("/home/mike/Documents/input0.txt") // open file
+	f, err := os.Open("/home/mike/Documents/aoc/day4-0.txt") // open file
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer f.Close() // remember to close the file at the end of the program
 
 	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanWords)
+	scanner.Split(bufio.ScanLines)
 
 	var lines = 0
 	for scanner.Scan() {
-		word := scanner.Text()
+		line := scanner.Text()
 		lines = lines + 1
-		fmt.Println(lines, safeParse(word))
+		if len(line) > 0 && len(line) < 15 {
+			words := strings.Split(line, " ")
+			fmt.Println("at line:", lines, "parsed:", safeParse(words[0]))
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -31,7 +35,7 @@ func main() {
 
 func safeParse(g string) int {
 	if i, err := strconv.Atoi(g); err != nil {
-		fmt.Println(err)
+		fmt.Println("safeParse fails -> ", err)
 		return -1
 	} else {
 		return i

dc789909f35bb4c12f6166cb3ef86549a407f51d
Mike Bochenek <mike.bochenek@gmail.com>
2022-03-18 23:02:44 +0100

    complete part I of day 11 - aoc 2021

--- go/aoc2021/day11.go
+++ go/aoc2021/day11.go
@@ -8,7 +8,7 @@ import (
 )
 
 const SIZE = 10
-const FILENAME = "day11-0"
+const FILENAME = "day11-1"
 
 func main() {
 	f, err := os.Open("/home/mike/Documents/aoc/" + FILENAME + ".txt")
@@ -34,27 +34,73 @@ func main() {
 
 	var flash = 0
 
-	for i := 0; i <= 4; i++ {
-		fmt.Println("\nafter", i, "iterations:")
-		printNicely(m)
+	for i := 0; i <= 100; i++ {
+		fmt.Println("\nafter step", i, "flashes:", flash)
+		//printNicely(m)
 
 		for j := 0; j < SIZE; j++ {
 			for k := 0; k < SIZE; k++ {
-				m[j][k] = m[j][k] + 1
+				m[j][k]++
 			}
 		}
 
-		
+		for n := 0; n < 40; n++ {
+			for j := 0; j < SIZE; j++ {
+				for k := 0; k < SIZE; k++ {
+					if m[j][k] > 9 {
+
+						flash += 1
+						m[j][k] = -100
+
+						if j > 0 && k > 0 && m[j-1][k-1] != 0 {
+							m[j-1][k-1]++
+						}
+						if j > 0 && m[j-1][k] != 0 {
+							m[j-1][k]++
+						}
+						if j > 0 && (k+1) < SIZE && m[j-1][k+1] != 0 {
+							m[j-1][k+1]++
+						}
+
+						if k > 0 && m[j][k-1] != 0 {
+							m[j][k-1]++
+						}
+						if (k+1) < SIZE && m[j][k+1] != 0 {
+							m[j][k+1]++
+						}
+
+						if (j+1) < SIZE && k > 0 && m[j+1][k-1] != 0 {
+							m[j+1][k-1]++
+						}
+						if (j+1) < SIZE && m[j+1][k] != 0 {
+							m[j+1][k]++
+						}
+						if (j+1) < SIZE && (k+1) < SIZE && m[j+1][k+1] != 0 {
+							m[j+1][k+1]++
+						}
+
+					}
+				}
+			}
+		}
+
+		for j := 0; j < SIZE; j++ {
+			for k := 0; k < SIZE; k++ {
+				if m[j][k] < 0 {
+					m[j][k] = 0
+				}
+			}
+		}
 	}
 
-	fmt.Println("\n\tflashes", flash)
+	//this was causing me pain!! fmt.Println("\n\tflashes", flash)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
 }
 
-func printNicely(m[SIZE][SIZE] int) {
+func printNicely(m [SIZE][SIZE]int) {
 	for i := 0; i < SIZE; i++ {
 		fmt.Println(m[i])
 	}

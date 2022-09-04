26d3a44880ab274ec3a5368d289bab32be366111
Mike Bochenek <mike.bochenek@gmail.com>
2022-09-04 10:01:03 +0200

    remind myself python

---
+++ python3/spyder-03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 09:56:13 2022
+@author: mike
+"""
+import pandas as pd
+
+df = pd.read_csv("/home/mike/Documents/aoc/day7-0.txt")
+print(df.info)

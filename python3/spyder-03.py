609798cebc32ad64644797079fe681f2b0df0af8
Mike Bochenek <mike.bochenek@gmail.com>
2022-09-04 10:19:52 +0200

    play with slicing

--- python3/spyder-03.py
+++ python3/spyder-03.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Sep  4 09:56:13 2022
+Created on Sun Sep  4 09:56:13 2022 - motived by https://codefol.io/posts/the-forty-year-programmer/
 @author: mike
+- a bit weird that auto-complete does not work as expected
+- nice new shortcut: control-enter (re)run current line (like SQL Developer)
 """
 import pandas as pd
+from datetime import datetime
 
-df = pd.read_csv("/home/mike/Documents/aoc/day7-0.txt")
-print(df.info)
+startTime = datetime.now()
+
+df = pd.read_csv("/home/mike/Documents/aoc/day7-0.txt") # print(df.info)
+
+df = pd.read_csv('/home/mike/ownCloud/Documents/backups/linkedin-export/Connections.csv')
+companies = (df.groupby(['Company']).size().sort_values(ascending=False))
+# print(df.tail(20))     # last 20
+# print(df.iloc[::2, 0]) # even/odd
+print(df.iat[12,0])      # https://note.nkmk.me/en/python-pandas-at-iat-loc-iloc/
+
+print(datetime.now() - startTime, 'linkedin-export shape:', df.shape) 

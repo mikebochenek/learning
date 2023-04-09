c95914c62a5ce60319c19b694cabb41db22f62cc
Mike Bochenek <mike.bochenek@gmail.com>
2023-04-09 09:08:24 +0200

    find median

--- python3/zillow_ch.py
+++ python3/zillow_ch.py
@@ -30,19 +30,22 @@ def score(s):
     d = calcP(df.query('MUNICIPALITY == "' + s + '" & YEAR == 2011'))
     return ((d['INCOME_GROUP_CODE'] * d['percentage']).sum())
 
+def findmedian(s):
+    d = calcP(df.query('MUNICIPALITY == "' + s + '" & YEAR == 2011'))
+    d['cumulative'] = d['percentage'].cumsum()
+    return d.query('cumulative > 50').iloc[0,0]
+
 u = df['MUNICIPALITY'].unique()
 for _u in (u):
     s = score(_u)
+    m = findmedian(_u)
     if (s > 999): #(s < 540):
-        print (' *** ', _u, s)
+        print (' *** ', _u, m, s)
     else:
         None
-        #print(_u, s)
+        #print(_u, m, s)
 
-print('\nHerrliberg', score('Herrliberg'))
-print('Rüschlikon', score('Rüschlikon'))
-print('Zollikon', score('Zollikon'))
-print('Küsnacht', score('Küsnacht '))
-print('Dietikon', score('Dietikon'))
-print('Wetzikon', score('Wetzikon'))
-print('Maur', score('Maur'))
+print('\nRüschlikon', score('Rüschlikon'))
+print('Dietikon', findmedian('Dietikon'), score('Dietikon'))
+print('Wetzikon', findmedian('Wetzikon'), score('Wetzikon'))
+print('Maur', findmedian('Maur'), score('Maur'))

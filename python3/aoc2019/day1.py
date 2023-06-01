97817f7bb7295d9750fd44b779d44127f61a58b9
Mike Bochenek <mike.bochenek@gmail.com>
2023-06-01 22:02:30 +0200

    playing around with python once again...

---
+++ python3/aoc2019/day1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 21:45:42 2023
+@author: mike
+"""
+
+def fuel(mass): # day1-1
+    return (int)(mass / 3) - 2
+
+assert(fuel(1969)==654)
+assert(fuel(100756)==33583)
+
+def recursive_fuel(mass): #day1-2
+    #print (mass, fuel(mass))
+    f = fuel(mass)
+    if (f <= 0):
+        return 0
+    else:
+        return recursive_fuel(f) + f
+
+assert(recursive_fuel(14) == 2)
+assert(recursive_fuel(1969) == 966)
+assert(recursive_fuel(100756) == 50346)
+
+
+with open('/tmp/day1.txt') as f:
+    lines = f.read().splitlines()
+
+sum = 0
+for n in lines:
+    sum += recursive_fuel(int(n))
+print ('day1',sum)

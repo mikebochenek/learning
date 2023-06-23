99eaf864e63742138623798a68596edd6568ba19
Mike Bochenek <mike.bochenek@gmail.com>
2023-06-23 22:27:56 +0200

    at least it compiles...

---
+++ python3/aoc2019/day4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: mike - Created on Fri Jun 23 22:22:40 2023 
+but somehow to lazy or can't think of code 
+"""
+
+def mc(i):
+    if i == 111111:
+        return True
+    return False
+
+assert (mc(111111) and False == mc(223450) and False == mc(123789))
+
+# 240298-784956

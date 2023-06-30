a49212d1e90e011de4a16721055c5b69418edc5e
Mike Bochenek <mike.bochenek@gmail.com>
2023-06-30 22:55:07 +0200

    starting coding again...

--- python3/aoc2019/day4.py
+++ python3/aoc2019/day4.py
@@ -13,7 +13,9 @@ def mc(i):
 assert (mc(111111) and False == mc(223450) and False == mc(123789))
 # 240298-784956
 
-
+for i in range(240298,784956):
+    None
+    
 # https://cryptography.io/en/latest/ -- also crypto101 would be fun
 from cryptography.fernet import Fernet
 key = Fernet.generate_key()

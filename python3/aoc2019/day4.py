6aa3427cc652a750bb4f4bb058fdedeeb4c3e276
Mike Bochenek <mike.bochenek@gmail.com>
2023-07-01 15:51:40 +0200

    finish day4 - part one at least...

--- python3/aoc2019/day4.py
+++ python3/aoc2019/day4.py
@@ -5,16 +5,34 @@
 but somehow to lazy or can't think of code 
 """
 
-def mc(i):
-    if i == 111111:
-        return True
-    return False
+def mc(i): #meets criteria?
+    digits = 6
+
+    # Two adjacent digits are the same (like 22 in 122345).
+    adjacent = False
+    
+    # Going from left to right, the digits never decrease; they only ever increase or stay the same
+    for j in range(0,digits-1):
+        if (str(i)[j] > str(i)[j+1]):
+            # print('digits decrease', i, j)
+            return False
+        if (str(i)[j] == str(i)[j+1]):
+            adjacent = True
+        
+    return adjacent
+
+    #if i == 111111:
+    #    return True
+    #return False
 
 assert (mc(111111) and False == mc(223450) and False == mc(123789))
-# 240298-784956
 
-for i in range(240298,784956):
-    None
+count = 0
+for i in range(240298,784956): # 240298-784956
+    if (mc(i)):
+        count = count + 1
+    
+print ("count", count)
     
 # https://cryptography.io/en/latest/ -- also crypto101 would be fun
 from cryptography.fernet import Fernet

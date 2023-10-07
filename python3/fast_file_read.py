137429cc4a572ce9e705882f74d1a6d67ea0a942
Mike Bochenek <mike.bochenek@gmail.com>
2023-10-07 21:43:56 +0200

    playing around with some open source data

--- python3/fast_file_read.py
+++ python3/fast_file_read.py
@@ -1,8 +1,18 @@
 # https://stackabuse.com/read-a-file-line-by-line-in-python/
+# https://docs.python-guide.org/writing/style/
+from datetime import datetime
 
-with open('/home/mike/Downloads-1/cities.csv') as f:
-#with open('/Users/mike/Downloads/travel-types-data.csv') as f:
-    for line in f:
-        print (line, end=' ')  # Do something with 'line'
-        
-# also see: https://docs.python-guide.org/writing/style/
+def testreadcsv(filename):
+    startTime = datetime.now()
+
+    count = 0
+    with open('/Users/mike/Downloads/' + filename + '.csv') as f:
+        for line in f:
+            count = count + len(line)
+            #len(line)
+            #print (line, end=' ')  # Do something with 'line'
+
+    print(datetime.now() - startTime, 'time taken to read small csv ', count, filename) 
+
+testreadcsv('WIR100OD1007') # https://data.stadt-zuerich.ch/dataset/fd_median_einkommen_kreis_od1007/download/WIR100OD1007.csv
+testreadcsv('10210') # https://data.bl.ch/api/v2/catalog/datasets/10210/exports/csv

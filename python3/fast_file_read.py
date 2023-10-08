0f646dce35c9a1bcb1081c3bae241074edd36efd
Mike Bochenek <mike.bochenek@gmail.com>
2023-10-08 15:40:59 +0200

    if file does not exist, try to download it

--- python3/fast_file_read.py
+++ python3/fast_file_read.py
@@ -1,12 +1,23 @@
 # https://stackabuse.com/read-a-file-line-by-line-in-python/
 # https://docs.python-guide.org/writing/style/
 from datetime import datetime
+import os.path
+import urllib.request
+
+files = {'WIR100OD1007': 'https://data.stadt-zuerich.ch/dataset/fd_median_einkommen_kreis_od1007/download/WIR100OD1007.csv', 
+         '10210': 'https://data.bl.ch/api/v2/catalog/datasets/10210/exports/csv'}
 
 def testreadcsv(filename):
     startTime = datetime.now()
 
     count = 0
-    with open('/Users/mike/Downloads/' + filename + '.csv') as f:
+    fullpath = '/Users/mike/Downloads/' + filename + '.csv'
+    if (os.path.isfile(fullpath) == False):
+        print ('lets download it then')
+        # urllib.request.urlretrieve(files.get(filename), fullpath)
+        # urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1108)>
+
+    with open(fullpath) as f:
         for line in f:
             count = count + len(line)
             #len(line)
@@ -14,5 +25,5 @@ def testreadcsv(filename):
 
     print(datetime.now() - startTime, 'time taken to read small csv ', count, filename) 
 
-testreadcsv('WIR100OD1007') # https://data.stadt-zuerich.ch/dataset/fd_median_einkommen_kreis_od1007/download/WIR100OD1007.csv
-testreadcsv('10210') # https://data.bl.ch/api/v2/catalog/datasets/10210/exports/csv
+for key, value in sorted(files.items()):
+    testreadcsv(key) # 

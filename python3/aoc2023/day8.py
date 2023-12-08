7cfd1a6289c4298eb10ee103d8f4da6502289095
Mike Bochenek <mike.bochenek@gmail.com>
2023-12-08 22:34:48 +0100

    still grinding part II ...

--- python3/aoc2023/day8.py
+++ python3/aoc2023/day8.py
@@ -12,32 +12,55 @@ def steps(filename):
     lines.remove('')
 
     m = {} # map
+    sset = set()
     for line in lines:
         l = line.split('=')
         key = l[0].strip()
         value = l[1].strip()
         #print ('adding', key, value)
+        if (key[2] == 'A'):
+            sset.add(key)
         m[key] = value
 
-    #print (lines, m.keys())
-    found = False
+    s = list(sset)
+    print (' --> ', len(s), s) #, lines, m.keys())
     count = 0
-    p = 'AAA'
-    while (found == False):
+    # p = '11A'
+    found = 0
+    while (found < len(s) and count <= 6000):
     #for count in range(0,len(ins)):
-        current = m[p]
-        #print (count, p, ' -> ', current)
-        if (ins[count % len(ins)] == 'L'):
-            p = current[1:4]
-        if (ins[count % len(ins)] == 'R'):
-            p = current[6:9]
-        count += 1
-        if (p == 'ZZZ'):
-            found = True
-            #print ('end!!', count)
+        found = 0
+        new_s = list(s)
+        for idx, p in enumerate(s):
 
-    return count
+            if (p[2] == 'Z'):
+                found += 1
+            if (found >= len(s)):
+                print ('end!!', count-1)
+                return (count-1) / len(s)
 
-print(steps('day8-0') == 2)
-print(steps('day8-0b') == 6)
+            current = m[p]
+            direction = ins[int(count / len(s)) % len(ins)] 
+            #print (count, p, ' -> ', current)
+            if (direction == 'L'):
+                p = current[1:4]
+            if (direction == 'R'):
+                p = current[6:9]
+            count += 1
+
+            new_s[idx] = p
+
+            #print (idx, s, direction, current, p, found)
+
+        s = new_s
+
+    return (count-1) / len(s)
+
+#print(steps('day8-0') == 2)
+#print(steps('day8-0b') == 6)
+print(steps('day8-0c') == 6)
 print('my answer', steps('day8-1'))
+
+# my answer 3.8333333333333335
+
+# 4 --> That's not the right answer. I

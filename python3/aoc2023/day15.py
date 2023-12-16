62f874ae727ea6720b3eca6a6fb6c1f1f0fa0c38
Mike Bochenek <mike.bochenek@gmail.com>
2023-12-16 14:22:26 +0100

    done part I successfully - suprisingly easy: about 12 minutes?

--- python3/aoc2023/day15.py
+++ python3/aoc2023/day15.py
@@ -1,20 +1,38 @@
+# start part I Sat Dec 16 14:08:23 CET 2023
+# done  part I Sat Dec 16 14:20:49 CET 2023
 
-def hash(filename):
+def weirdhash(str):
+    cv = 0
+    for s in str:
+        cv += ord(s)
+        cv *= 17
+        cv = cv % 256
+
+        #print (s, cv)
+
+    return cv
+
+def seqsum(filename):
 
     with open('/home/mike/ownCloud/Documents/aoc2023/'+filename+'.txt') as f:
         lines = f.read().splitlines()
 
-    sum = 0
+    mysum = 0
 
     m = {} # map
     for line in lines:
         l = line.split(',')
         for s in l:
+
+            mysum += weirdhash(s)
+
             key = s[0].strip()
             value = s[1].strip()
-            print ('adding', key, value)
+            # print ('adding', key, value)
 
+    print (filename, '->', mysum)
+    return mysum
 
-    return sum / len(s)
-
-print(hash('day15-0') == 1320)
+print(weirdhash('HASH'))
+print(seqsum('day15-0') == 1320)
+print(seqsum('day15-1'))

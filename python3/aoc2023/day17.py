10bdb36397ce0895cdf4dd057b6c352c97ea97b1
Mike Bochenek <mike.bochenek@gmail.com>
2023-12-17 10:04:04 +0100

    gotta keep going - day17... watching Jamie Vardy bio was fun

---
+++ python3/aoc2023/day17.py
@@ -0,0 +1,12 @@
+def least(filename):
+
+    with open('/home/mike/ownCloud/Documents/aoc2023/'+filename+'.txt') as f:
+        lines = f.read().splitlines()
+
+    for line in lines:
+        min = 0
+ 
+    return 0
+
+print(least('day17-0') == 102)
+# print(least('day17-1'))

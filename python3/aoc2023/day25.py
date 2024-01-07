66cb8d58dc9b95824e418b39b0bbb28403178a8f
Mike Bochenek <mike.bochenek@gmail.com>
2024-01-07 09:40:09 +0100

    day25 - barely started some time back

---
+++ python3/aoc2023/day25.py
@@ -0,0 +1,13 @@
+def mult(filename):
+
+    with open('/Users/mike/ownCloud/Documents/aoc2023/'+filename+'.txt') as f:
+        lines = f.read().splitlines()
+
+    for line in lines:
+        
+        min = 0
+ 
+    return 0
+
+print(mult('day25-0') == 54)
+# print(least('day17-1'))

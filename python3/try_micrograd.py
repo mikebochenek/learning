968b81121b40b03541ef3b78537cb37ece3eb763
Mike Bochenek <mike.bochenek@gmail.com>
2024-03-17 21:00:07 +0100

    little brainteaser...

--- python3/try_micrograd.py
+++ python3/try_micrograd.py
@@ -5,3 +5,17 @@ model = MLP(2, [4, 1])
 
 print (model)
 
+# millennium brainteaser
+def f(n):
+    if n <= 1:
+        return 1/6
+    else:
+        return f(n-1) + (1 + 1 / (n*(4*n-1)*(4*n-2)) )
+
+def g(n):
+    if n <= 1:
+        return 1/30
+    else:
+        return g(n-1) + (1 + 1 / (n*(4*n+1)*(4*n+2)) )
+
+print (f(2024) + 3 - g(2024))

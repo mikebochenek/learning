bfb3bcf50ba07a2b4c2ca707a827f71687b8ac5a
Mike Bochenek <mike.bochenek@gmail.com>
2024-03-17 15:39:21 +0100

    graphing works - not pretty...

--- python3/fitness_stats.py
+++ python3/fitness_stats.py
@@ -18,14 +18,29 @@ import numpy as np; np.random.seed(sum(map(ord, 'calmap')))
 all_days = pd.date_range('1/15/2014', periods=700, freq='D')
 days = np.random.choice(all_days, 500)
 events = pd.Series(np.random.randn(len(days)), index=days)
-# calmap.yearplot(events, year=2015)
+#calmap.yearplot(events, year=2015)
 
 # example #2
 import july
 from july.utils import date_range
 dates = date_range("2024-01-01", "2024-08-31")
-data = np.random.randint(0, 14, len(dates))
-# july.heatmap(dates, data, title='Fitness Activity', cmap="github")
+data = np.random.randint(0, 1, len(dates))
 
-events = pd.to_datetime(df.head(8).get('date'),format="%d.%m.%Y")
+# events = pd.to_datetime(df.head(8).get('date'),format="%d.%m.%Y")
+from datetime import datetime
+start = datetime.strptime('01.01.2024', "%d.%m.%Y")
+for d in df.iterrows(): # it ain't pretty, but does the job 
+    date = d[1][2]
+    if (isinstance(date, str) and ('2024' in date)):
+        dObj = datetime.strptime(date, "%d.%m.%Y")
+        diff = dObj - start 
+        print('date', dObj, type(date), d[1][4], diff.days)
+        
+        if  ('football' == d[1][4]):
+            data[diff.days] = 3
+        if  ('running' == d[1][4]):
+            data[diff.days] = 2
+        if  ('swimming' == d[1][4]):
+            data[diff.days] = 1
 
+july.heatmap(dates, data, title='Fitness Activity', cmap="github")

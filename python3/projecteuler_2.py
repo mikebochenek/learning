c3b04c6a2fbc199bc25217c8c138fef51b0422e5
Mike Bochenek <mike.bochenek@gmail.com>
2024-05-28 08:22:25 +0200

    fun math coding

---
+++ python3/projecteuler_2.py
@@ -0,0 +1,11 @@
+# https://projecteuler.net/problem=885
+
+def f885(n):
+    return 334
+def s885(n):
+    ans = 1 % 1123455689
+    return ans
+
+print (f885(3403))
+print (s885(1)) # should be 45
+print (s885(5)) # should be 1543545675

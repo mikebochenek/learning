1be97db4d24f197f8ec0574dbcae77bae45295a3
Mike Bochenek <mike.bochenek@gmail.com>
2024-05-31 21:07:33 +0200

    a tad bit of puzzle coding

--- python3/projecteuler_2.py
+++ python3/projecteuler_2.py
@@ -1,11 +1,19 @@
 # https://projecteuler.net/problem=885
 
 def f885(n):
-    return 334
+    s_n = str(n)
+    digits = []
+    for s in s_n: # kinda brute force
+        if (s != '0'):
+            digits.append(s) 
+    digits.sort()
+    s_n = ''.join(digits) # convert digits list into a string
+    return int(s_n) # return integer     # print (digits, s_n)  
+
 def s885(n):
     ans = 1 % 1123455689
     return ans
 
-print (f885(3403))
+assert (334 == (f885(3403)))
 print (s885(1)) # should be 45
 print (s885(5)) # should be 1543545675

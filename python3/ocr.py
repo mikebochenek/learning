2044767c7d4f53dd0310b174d266853a8ad1610e
Mike Bochenek <mike.bochenek@gmail.com>
2024-08-21 19:17:40 +0200

    new version of ocr

--- python3/ocr.py
+++ python3/ocr.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Aug 18 17:25:26 2024
-
 @author: mike
 """
 
@@ -19,7 +18,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/mike/Documents/prestoch-ae
 # originally from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/vision/cloud-client/detect/detect.py
 # needs export GOOGLE_APPLICATION_CREDENTIALS=/home/mike/Documents/presto-bites/prestoch-ae89fcfa026f.json
 # [START vision_text_detection_gcs]
-def detect_text_uri(uri):
+def detect_text_uri_v0(uri):
     """Detects text in the file located in Google Cloud Storage or on the Web."""
     startTime = datetime.now()
     client = vision.ImageAnnotatorClient()
@@ -49,4 +48,84 @@ def detect_text_uri(uri):
 
 # ImportError: cannot import name 'image_annotator' from 'google.cloud.vision_v1.types' (/home/mike/anaconda3/lib/python3.10/site-packages/google/cloud/vision_v1/types.py)
 
-detect_text_uri('https://bochenek.ch/media/20240202_153004.jpg')
+#detect_text_uri('https://bochenek.ch/media/20240202_153004.jpg')
+
+
+
+"""
+This application demonstrates how to perform basic operations with the Google Cloud Vision API.
+
+pip install google-cloud-vision==3.4.2
+pip install google-cloud-storage==2.9.0
+
+https://github.com/GoogleCloudPlatform/python-docs-samples/blob/main/vision/snippets/detect/detect.py
+"""
+
+
+# [START vision_text_detection]
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    # [START vision_python_migration_text_detection]
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    #print("Texts:")
+
+    for text in texts:
+        #print(f'\n"{text.description}"')
+        return (text.description)
+
+        vertices = [
+            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
+        ]
+
+        # print("bounds: {}".format(",".join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+# [END vision_text_detection]
+
+
+# [START vision_text_detection_gcs]
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web."""
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print("Texts:")
+
+
+    for text in texts:
+        print(f'\n"{text.description}"')
+
+        vertices = [
+            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
+        ]
+
+        print("bounds: {}".format(",".join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+# [END vision_text_detection_gcs]
+
+t = detect_text('/home/mike/Downloads/20240202_153004.jpg')
+print(t)

de0fc1c6cb95c44715441cfe95252c6981c83521
Mike Bochenek <mike.bochenek@gmail.com>
2024-08-21 19:26:24 +0200

    vertex parametrized

--- python3/try_vertexai.py
+++ python3/try_vertexai.py
@@ -2,17 +2,10 @@ import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Part
 import vertexai.preview.generative_models as generative_models
 
-def generate():
+def generate(q):
   vertexai.init(project="smarty-414711", location="us-central1")
   model = GenerativeModel("gemini-pro")
-  responses = model.generate_content(
-    """Read the below multiple choice question carefully, and provide the best answer:
-
-	Git. You first sum 2 and two, and then multiply the result by four. What is the outcome of this operation?
-	(a) 16
-	(b) 10
-	(c) sixteen
-	(d) A failure because you cannot add before multiplication""",
+  responses = model.generate_content(q,
     generation_config={
         "max_output_tokens": 2048,
         "temperature": 0.9,
@@ -31,4 +24,11 @@ def generate():
     print(response.text, end="")
 
 
-generate()
+generate(
+    """Read the below multiple choice question carefully, and provide the best answer:
+
+  Git. You first sum 2 and two, and then multiply the result by four. What is the outcome of this operation?
+  (a) 16
+  (b) 10
+  (c) sixteen
+  (d) A failure because you cannot add before multiplication""")

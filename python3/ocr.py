6dea475959aca0f96d253090fe834e8ffbfc93be
Mike Bochenek <mike.bochenek@gmail.com>
2024-08-23 20:46:30 +0200

    remove unused parser based on url

--- python3/ocr.py
+++ python3/ocr.py
@@ -97,35 +97,5 @@ def detect_text(path):
 # [END vision_text_detection]
 
 
-# [START vision_text_detection_gcs]
-def detect_text_uri(uri):
-    """Detects text in the file located in Google Cloud Storage or on the Web."""
-    from google.cloud import vision
-
-    client = vision.ImageAnnotatorClient()
-    image = vision.Image()
-    image.source.image_uri = uri
-
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print("Texts:")
-
-
-    for text in texts:
-        print(f'\n"{text.description}"')
-
-        vertices = [
-            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
-        ]
-
-        print("bounds: {}".format(",".join(vertices)))
-
-    if response.error.message:
-        raise Exception(
-            "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(response.error.message)
-        )
-# [END vision_text_detection_gcs]
-
 # t = detect_text('/home/mike/Downloads/20240202_153004.jpg')
 # print(t)

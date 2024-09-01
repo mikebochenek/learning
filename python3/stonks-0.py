c608c38ccb348bfb16080f708963bf2507a1a2fa
Mike Bochenek <mike.bochenek@gmail.com>
2024-09-01 10:10:43 +0200

    trying to create pickle cache files, but getting weird TypeError: cannot pickle '_thread.lock' object

--- python3/stonks-0.py
+++ python3/stonks-0.py
@@ -7,6 +7,8 @@ Created on Sat Aug 31 11:53:00 2024 based on https://algotrading101.com/learn/ya
 
 import yfinance as yf
 import logging
+import pickle
+import os
 
 logging.basicConfig(level=logging.INFO,
     # filename="/tmp/app.log", # ideally https://docs.python.org/3/library/logging.handlers.html#logging.handlers.RotatingFileHandler
@@ -14,11 +16,28 @@ logging.basicConfig(level=logging.INFO,
     style="{"
     )
 
-def get_data(st): #get if it doesn't exist...
-    d = yf.Ticker(st)
-    logging.info(d.info)
-    return d
+# https://machinelearningmastery.com/a-gentle-introduction-to-serialization-for-python/
+# neat thing about stack-overflow coding, is that one discovers interesting areas of the web...
+def get_data(st): #get if it doesn't exist - pickle serialization
+    path = '/home/mike/Documents/opendata/ticker/'
+    pickle_filename = path + st + '.pickle'
+
+    if (os.path.isfile(pickle_filename)):
+        logging.info('load local ticker cache' + pickle_filename)
+        with open(pickle_filename, "rb") as infile:
+            ticker_reconstructed = pickle.load(infile)
+            return ticker_reconstructed
+    else:
+        d = yf.Ticker(st)
+        logging.info(d.info)
+
+        #with open(pickle_filename , "wb") as outfile:
+            # pickle.dump(d, outfile)
+            #logging.info('save to local pickle cache' + pickle_filename)
+
+        return d
     
+
 # msft = get_data("MSFT")
 nvda = get_data("NVDA")
 ubsg = get_data("UBSG")

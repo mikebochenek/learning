c02eae7f9deaf9e84e19e706c1a5dde31f54aac2
Mike Bochenek <mike.bochenek@gmail.com>
2024-09-22 09:20:31 +0200

    basic physics - speed

---
+++ python3/physics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+motivated by study with Luke & https://github.com/rougier/scientific-visualization-book
+"""
+
+def velocity(t, t0, s, s0):
+    return (s-s0) / (t-t0)
+
+def convert_to_km_per_h(v):
+    return 0
+
+print(velocity(41.8,32,100,0), 'm/s')
+
+def strecke_at(t, t0, s0, v):
+    return v * (t-t0) + s0
+
+print(strecke_at(6, 1, 0, 10))

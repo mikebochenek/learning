06a9e3f31edcfe8f5ea2fd9d8a66a22e16af36d6
Mike Bochenek <mike.bochenek@gmail.com>
2024-11-25 16:17:32 +0100

    code cleanup only

--- python3/transport_opendata.py
+++ python3/transport_opendata.py
@@ -4,6 +4,10 @@
 Created on Thu May  9 14:41:33 2024 
 https://transport.opendata.ch/docs.html
 https://github.com/OpendataCH/Transport/blob/master/web/examples/connections.php
+http://transport.opendata.ch/v1/locations?type=address&query=Z%C3%BCrich,+F%C3%B6rrlibuckstr.+60
+
+or alternative:
+https://search.ch/timetable/api/route.json?from=Einsiedeln&to=Z%C3%BCrich,+F%C3%B6rrlibuckstr.+60
 
 @author: mike
 """
@@ -14,13 +18,12 @@ from datetime import datetime
 
 locations = [8576202, # Zollikon, Felbenstrasse
     8503059, # Zürich Stadelhofen, Bahnhof
-    "8591329"] # "Zürich, Saalsporthalle" 
+    8591329] # "Zürich, Saalsporthalle" 
 
 ## TODO: would be nice to do lazy-fetch (saving locally, also for debugging..)
 def fetchurl(URL):
     page = requests.get(URL)
-    connection_data = json.loads(page.text)
-    
+    connection_data = json.loads(page.text)    
     # print(json.dumps(connection_data, indent=4))
     
     minimum_duration = 1000;
@@ -28,7 +31,6 @@ def fetchurl(URL):
         d = c.get('duration')
         m = datetime.strptime(d[3:], '%H:%M:%S')
         print (' _ minute:', m.minute)
-        
         if (m.minute < minimum_duration):
             minimum_duration = m.minute
         
@@ -53,8 +55,6 @@ if len(sys.argv) > 1:
 #duration = fetchurl_S(sample)
 #print ('(minimum)_duration:', duration, sample)
 
-print ('  start:', start)
-
 destinations = ['Minervastrasse 14, Zürich',
     'Zürich, Therese-Giehse-Str. 6',
     'Rämistrasse 101, Zürich',
@@ -63,6 +63,7 @@ destinations = ['Minervastrasse 14, Zürich',
     'Flurstrasse 68,8048 Zürich']
 
 final_list = []
+print ('  start:', start)
 for dest in destinations:
     sample = 'https://search.ch/timetable/api/route.json?from='+start+'&to='+dest
     duration = fetchurl_S(sample)
@@ -70,7 +71,3 @@ for dest in destinations:
     final_list.append(duration)
 
 print ('\n', final_list)
-# or is this even better?
-# https://search.ch/timetable/api/route.json?from=Einsiedeln&to=Z%C3%BCrich,+F%C3%B6rrlibuckstr.+60
-
-# http://transport.opendata.ch/v1/locations?type=address&query=Z%C3%BCrich,+F%C3%B6rrlibuckstr.+60

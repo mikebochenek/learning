6ec7a1f787c841899690ad2abf2e8fb7f8744d2a
Mike Bochenek <mike.bochenek@gmail.com>
2025-01-18 22:07:52 +0100

    ensure that golang is also working in windows

--- go/hello.go
+++ go/hello.go
@@ -10,6 +10,6 @@ import (
 
 func main() {
 	now := time.Now()
-	fmt.Printf("\nhello, world with fmt [ %v ]\n", now)
-	log.Printf("\nhello, world with log [ %v ]\n", now)
+	fmt.Printf("\nhello world! with fmt [ %v ]\n", now)
+	log.Printf("\nhello world! with log [ %v ]\n", now)
 }

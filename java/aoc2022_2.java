4c93efde937e33b2aef3f01c3130f67db1c56dab
unknown <mike@bochenek.ca>
2025-11-30 14:23:36 +0100

    look at me go - part II also done shortly afterwards

--- java/aoc2022_2.java
+++ java/aoc2022_2.java
@@ -39,13 +39,30 @@ public class aoc2022_2 { // just keep hacking, and testing the borders
             } else {
                 score += 3; // tie 
             }
-            //out.println(line + " -> " + score);
-
             ans += score;            
+
+            score = 0;
+            // part II: X need to lose, Y need a draw, and Z means need to win.
+            if ("A".equals(p[0])) { // rock
+                if ("X".equals(p[1])) score += 0 + 3; // need to lose, pick scissors
+                if ("Y".equals(p[1])) score += 3 + 1; // need to tie, pick rock
+                if ("Z".equals(p[1])) score += 6 + 2; // need to win, pick paper
+            } else if ("B".equals(p[0])) { // paper 
+                if ("X".equals(p[1])) score += 0 + 1; // need to lose, pick rock
+                if ("Y".equals(p[1])) score += 3 + 2; // need to tie, pick paper
+                if ("Z".equals(p[1])) score += 6 + 3; // need to win, pick scissors
+            } else if ("C".equals(p[0])) { // scissors 
+                if ("X".equals(p[1])) score += 0 + 2; // need to lose, pick paper
+                if ("Y".equals(p[1])) score += 3 + 3; // need to tie, pick scissors
+                if ("Z".equals(p[1])) score += 6 + 1; // need to win, pick rock
+            } // can't believe I cut and pasted those 3 IF statements above
+
+            ans2 += score;
+            //out.println(line + " -> " + score);
         }
 
         out.println("part I correct?  " + (ans == 15632)); // 15 for t.txt (test input)
-        out.println("part II correct? " + (ans2 == 199172));
+        out.println("part II correct? " + (ans2 == 14416)); // 11 for test input
         out.println("\tsize: " + lines.size() + " part I: " + ans + " part II: " + ans2 + "\n");
     }
 }

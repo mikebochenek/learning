4af5f02cffbc72ff81d6282e7f6c3bd3e1afdf72
unknown <mike@bochenek.ca>
2025-12-03 18:19:26 +0100

    just realized that the approach is wrong...

--- java/aoc2025_3.java
+++ java/aoc2025_3.java
@@ -14,33 +14,41 @@ public class aoc2025_3 {
         }
     }
 
-    public static int max(String input) {
+    public static long max(String input, int depth) {
         char[] charArray = input.toCharArray();
         charArray[charArray.length - 1] = '0'; // only needed for left digit
         Arrays.sort(charArray);
         String reverse = new StringBuffer(new String(charArray)).reverse().toString();
         char maxDigit = reverse.charAt(0);
 
-        int idx = input.indexOf(maxDigit);
-        //out.println(idx);
-        input = input.substring(idx+1);
-        //out.println(" --> new input " + input);
-        charArray = input.toCharArray();
-        Arrays.sort(charArray);
-        reverse = new StringBuffer(new String(charArray)).reverse().toString();
-        //System.out.println(input + " --> " + maxDigit +  reverse.charAt(0));  
-        int minDigit = reverse.charAt(0);
+        String ans = ""+maxDigit;
+        for (int i = 1; i < depth; i++) {
+            int idx = input.indexOf(maxDigit);
+            input = input.substring(idx+1);
+            //out.println(idx + " --> new input " + input);
+            charArray = input.toCharArray();
 
-        return Character.getNumericValue(maxDigit) * 10 + Character.getNumericValue(minDigit);
+            Arrays.sort(charArray);
+            reverse = new StringBuffer(new String(charArray)).reverse().toString();
+            //System.out.println(input + " --> " + maxDigit +  reverse.charAt(0));  
+            int minDigit = reverse.charAt(0);
+
+            //out.println("ans:" + ans + " mindigit " + Character.getNumericValue(minDigit));
+            ans += (""+Character.getNumericValue(minDigit));
+        }
+
+        return Long.parseLong(ans); 
+        //Character.getNumericValue(maxDigit) * 10 + Character.getNumericValue(minDigit);
     }
 
-    public static int calc(String fn) {
+    public static long calc(String fn, int d) {
         List<String> lines = read("c:\\dev\\data\\aoc\\" + fn);
-        int ans = 0;
+        long ans = 0;
 
         for (String line : lines) {
-            //out.println("->" + line);
-            ans += max(line);
+            long a = max(line, d);
+            //out.println(line + " -> " + a);
+            ans += a;
         }
         
         out.println("\tsize: " + lines.size() + " ans: " + ans + "\n");
@@ -49,9 +57,11 @@ public class aoc2025_3 {
     }
 
     public static void main(String[] args) { 
-        
-        out.println("part I correct?  " + (calc("2025_day3t.txt") == 357)); 
-        out.println("part I answer  " + (calc("2025_day3.txt") == 16842)); 
+        out.println("------");        
+        out.println("part I correct?  " + (calc("2025_day3t.txt", 2) == 357)); 
+        //out.println("part I answer  "   + (calc("2025_day3.txt", 2) == 16842)); 
+
+        out.println("part II correct? " + (calc("2025_day3t.txt", 12) == 3121910778619l)); 
 
         //out.println("part II correct? " + (ans2 == 14416)); 
     }

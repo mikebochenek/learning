ddddb263590d55fc3128c0f34bf4f47ffa025892
unknown <mike@bochenek.ca>
2025-12-03 17:02:15 +0100

    part I completed,.. in an ugly way

--- java/aoc2025_3.java
+++ java/aoc2025_3.java
@@ -14,15 +14,45 @@ public class aoc2025_3 {
         }
     }
 
-    public static void main(String[] args) { 
-        List<String> lines = read("c:\\dev\\data\\aoc\\2025_day3t.txt");
-        int ans = 0, ans2 = 0;
+    public static int max(String input) {
+        char[] charArray = input.toCharArray();
+        charArray[charArray.length - 1] = '0'; // only needed for left digit
+        Arrays.sort(charArray);
+        String reverse = new StringBuffer(new String(charArray)).reverse().toString();
+        char maxDigit = reverse.charAt(0);
+
+        int idx = input.indexOf(maxDigit);
+        //out.println(idx);
+        input = input.substring(idx+1);
+        //out.println(" --> new input " + input);
+        charArray = input.toCharArray();
+        Arrays.sort(charArray);
+        reverse = new StringBuffer(new String(charArray)).reverse().toString();
+        //System.out.println(input + " --> " + maxDigit +  reverse.charAt(0));  
+        int minDigit = reverse.charAt(0);
+
+        return Character.getNumericValue(maxDigit) * 10 + Character.getNumericValue(minDigit);
+    }
+
+    public static int calc(String fn) {
+        List<String> lines = read("c:\\dev\\data\\aoc\\" + fn);
+        int ans = 0;
 
         for (String line : lines) {
+            //out.println("->" + line);
+            ans += max(line);
         }
+        
+        out.println("\tsize: " + lines.size() + " ans: " + ans + "\n");
 
-        out.println("part I correct?  " + (ans == 15632)); // 15 for t.txt (test input)
-        out.println("part II correct? " + (ans2 == 14416)); // 11 for test input
-        out.println("\tsize: " + lines.size() + " part I: " + ans + " part II: " + ans2 + "\n");
+        return ans;
+    }
+
+    public static void main(String[] args) { 
+        
+        out.println("part I correct?  " + (calc("2025_day3t.txt") == 357)); 
+        out.println("part I answer  " + (calc("2025_day3.txt") == 16842)); 
+
+        //out.println("part II correct? " + (ans2 == 14416)); 
     }
 }

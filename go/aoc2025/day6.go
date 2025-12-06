478faf4a7c4400ba98a72e6f7b751b7fdf97c290
unknown <mike@bochenek.ca>
2025-12-06 09:52:55 +0100

    part II was not too bad this time.. small tricks, but somehow brain computed

--- go/aoc2025/day6.go
+++ go/aoc2025/day6.go
@@ -12,7 +12,8 @@ func main() {
 	now := time.Now()
 	fmt.Println(now, "\n----> ", (4277556 == calc("2025_day6t.txt", 1)))
 	fmt.Println("\n----> ", (6299564383938 == calc("2025_day6.txt", 1)))
-	//fmt.Println("\n----> ", (43 == calc("2025_day4t.txt", 2)))
+	fmt.Println("\n----> ", (3263827 == calc("2025_day6t.txt", 2)))
+	fmt.Println("\n----> ", (11950004808442 == calc("2025_day6.txt", 2)))
 }
 func calc(fn string, part int) int {
 	f, err := os.Open("c:\\dev\\data\\aoc\\" + fn) 
@@ -46,24 +47,86 @@ func calc(fn string, part int) int {
 
 	for i := 0; i < len(ops); i+=1 {
 		total := 0
+
 		for j := 0; j < len(lines)-1; j+=1 {
 			line := lines[j]
-			numbers := removeBlanks(strings.Split(line, " "))
-			v := safeParse(numbers[i])
-			if (ops[i] == "+") {
-				//fmt.Println("add", v)
-				total += v
-			}
-			if (ops[i] == "*") {
-				if total == 0 {
-					total = 1
+
+			if (part == 1) {
+
+				numbers := removeBlanks(strings.Split(line, " "))
+				v := safeParse(numbers[i])
+				if (ops[i] == "+") {
+					//fmt.Println("add", v)
+					total += v
 				}
-				//fmt.Println("multiply", v)
-				total *= v
+				if (ops[i] == "*") {
+					if total == 0 {
+						total = 1
+					}
+					//fmt.Println("multiply", v)
+					total *= v
+				}
+			} else if (part == 2) {
+
+				// that won't work! idx := strings.Index(lastLine, "")
+			}
+
+		}
+
+		if (part == 1) {
+			count += total
+		}
+	}
+
+	if (part == 2) {
+		column := len(ops) - 1
+
+		add := 0
+		mul := 1
+
+		for i:=0; i < len(lines[0]); i+=1 {
+
+			idx := len(lines[0])-i-1
+			str := ""
+			for j:=0; j < len(lines)-1; j+=1 {
+				line := lines[j]
+				v := safeParse(string(line[idx]))
+				str += strconv.Itoa(v)
+				//fmt.Println(idx, line, " --\t", v, str)
+			}
+
+			//TODO careful with 800 and 100 ! they are wrong - input has only 4 lines, and no zeros
+			str = strings.Replace(str, "0", "", -1)
+
+			digit := safeParse(str)
+			//fmt.Println(idx, " --\t", "column", column, digit, "\t", add, mul)
+
+			if (digit != 0) {
+				add += digit
+				mul = mul * digit
+			}
+
+			if (digit == 0 || idx == 0) { //check if only zeros 
+
+				if (ops[column] == "+") {
+					count += add
+					//fmt.Println("adding", add)
+
+				} else if (ops[column] == "*") {
+					count += mul
+					//fmt.Println("multiply", mul)
+				} else {
+					fmt.Println("undefined ops?")
+				}
+
+				add = 0
+				mul = 1
+
+				column = column - 1
 			}
 		}
-		count += total
 	}
+
 
 	fmt.Println(part, fn, "about to return", count)
 	return count 
@@ -83,8 +146,8 @@ func removeBlanks(s []string) []string {
 
 func safeParse(g string) int {
 	if i, err := strconv.Atoi(g); err != nil {
-		fmt.Println("safeParse fails -> ", err)
-		return -1
+		//fmt.Println("safeParse fails -> ", err)
+		return 0
 	} else {
 		return i
 	}

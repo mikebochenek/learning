3a7ec5ac40fa70e5d381471adac1e55ab641de90
unknown <mike@bochenek.ca>
2025-12-06 08:53:04 +0100

    template for 2025, although run on save doesn't recognize python as a command..

---
+++ python3/aoc2025/day6.py
@@ -0,0 +1,11 @@
+
+def calc_total(filename):
+    total_cal_value = 0
+
+    with open("c:\\dev\\data\\aoc\\" + filename) as f:
+        lines = f.read().splitlines()
+
+
+    return 0
+
+calc_total()

1bb8ad2fc77756b60700f8afdb2b99a62c8837c8
unknown <mike@bochenek.ca>
2025-12-10 11:13:59 +0100

    day10

---
+++ python3/aoc2025/day10.py
@@ -0,0 +1,19 @@
+import datetime
+
+def calc_total(filename, part):
+
+    with open("c:\\dev\\data\\aoc\\" + filename) as f:
+        lines = f.read().splitlines()
+
+    c = 0
+    for line in lines:
+        # print(line)
+        pass
+
+    print (filename, " -> about to return", c)
+    return c
+
+print (datetime.datetime.now(), ".. day 10 ..")
+print (21 == calc_total("2025_day10t.txt", 1))
+#print (1598 == calc_total("2025_day7.txt", 1))
+#print (40 == calc_total("2025_day7t.txt", 2))

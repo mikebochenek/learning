ed7e2d2949c59f17b8cd4cf5ab67024120af5d77
unknown <mike@bochenek.ca>
2025-12-12 17:25:50 +0100

    keep trying, although I'm not sure when the contest closes - re: scoring

--- python3/aoc2025/day12.py
+++ python3/aoc2025/day12.py
@@ -7,16 +7,14 @@ def calc_total(filename, part):
 
     c = 0
     for line in lines:
-        s = line.split(":")
-        paths = s[1].split(" ")
-        print(s[0], " --> ", paths)
+        print(line)
         pass
 
     print (filename, " -> about to return", c)
     return c
 
-print (datetime.datetime.now(), ".. day 11 ..")
-print (5 == calc_total("2025_day11t.txt", 1))
+print (datetime.datetime.now(), ".. day 12 ..")
+print (2 == calc_total("2025_day12t.txt", 1))
 #print (1598 == calc_total("2025_day7.txt", 1))
 #print (40 == calc_total("2025_day7t.txt", 2))
 

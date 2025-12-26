2b8167b0b85d109e20fbfd91f9e4e11243c405d4
unknown <mike@bochenek.ca>
2025-12-26 08:15:47 +0100

    random number guesser based on tutorial https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html

--- rust/guessing/src/main.rs
+++ rust/guessing/src/main.rs
@@ -1,7 +1,17 @@
 use std::io;
+use rand::Rng;
 
+/* https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html 
+   (base) PS C:\dev\code\learning\rust\guessing> dir .\target\debug\ 
+   cargo run 
+   cargo build 
+   cargo test */
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 

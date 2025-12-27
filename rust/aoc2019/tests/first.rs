3538603e928b6bcd515a651a7931840d2912fe6a
Mike Bochenek <mike.bochenek@gmail.com>
2025-12-27 08:40:39 +0100

    where to place test code and how to run it? important to know!

---
+++ rust/aoc2019/tests/first.rs
@@ -0,0 +1,15 @@
+// https://doc.rust-lang.org/book/ch11-03-test-organization.html
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

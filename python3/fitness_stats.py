32fc7c0a32a6b9879f79496a72ce1e10cf855edf
Mike Bochenek <mike.bochenek@gmail.com>
2026-01-02 08:19:03 +0100

    prep for 2026 - although not sure if I will track this so much now that is really a HABIT!

--- python3/fitness_stats.py
+++ python3/fitness_stats.py
@@ -29,16 +29,17 @@ events = pd.Series(np.random.randn(len(days)), index=days)
 # example #2
 import july
 from july.utils import date_range
-dates = date_range("2025-01-01", "2025-12-31")
+dates = date_range("2026-01-01", "2026-12-31")
 data = np.random.randint(0, 1, len(dates))
 
 # events = pd.to_datetime(df.head(8).get('date'),format="%d.%m.%Y")
 from datetime import datetime
 types = ['','biking', 'swimming','running','football', 'basketball']
-start = datetime.strptime('01.01.2025', "%d.%m.%Y")
+start = datetime.strptime('01.01.2026', "%d.%m.%Y")
 
 count2024=0
 count2025=0
+count2026=0
 
 for d in df.iterrows(): # it ain't pretty, but does the job 
     date = d[1].iloc[2]  # d[1][2]
@@ -47,6 +48,9 @@ for d in df.iterrows(): # it ain't pretty, but does the job
 
     if (isinstance(date, str) and ('2025' in date)):
         count2025 += 1
+
+    if (isinstance(date, str) and ('2026' in date)):
+        count2026 += 1
         dObj = datetime.strptime(date, "%d.%m.%Y")
         diff = dObj - start 
         idx = types.index(d[1][4]) if d[1][4] in types else 1 # default empty to easy (biking) https://claude.ai/chat/dc7f2b9d-4631-43d7-91cf-36dde1c455f7
@@ -57,8 +61,9 @@ july.heatmap(dates, data, title='Fitness Activity', cmap="github") #cmap="golden
 
 # plt.savefig(outfilename)
 
-print ('\t', datetime.now(), '2025:', count2025, '2024:', count2024, 'total fitness entries:', len(df), 
+print ('\t', datetime.now(), '2026:', count2026, '2025:', count2025, '2024:', count2024, 'total fitness entries:', len(df), 
     ' - 2024 avg:', round((count2024*1.0/(52-7)), 2), '2025 avg:', round((count2025*1.0/(52)), 2),
+    '2026 avg:', round((count2026*1.0/(52)), 2), #TODO 52 needs manual adjustment!
     '\n\t', int((datetime.now() - startTime).total_seconds() * 1000), 
     'ms expired, on', platform.system(), platform.release(), 'created:', outfilename)
 # max 2025 04.09.2025 (week 36) TODO - need to update manually above

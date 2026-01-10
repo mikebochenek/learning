670fe92c15fc85f54c4bc5b66cb9919513393fb3
unknown <mike@bochenek.ca>
2026-01-10 09:09:29 +0100

    little daily status script

---
+++ python3/daily_status.py
@@ -0,0 +1,25 @@
+# idea is to print + email myself with something like:  if I commit today, it will make my streak X
+# ? does digital ocean work with emails atm?  days since May 30, 2011 ? 
+also = "2025: 162 2024: 125 (total : 350)  - 2024 avg: 2.78 2025 avg: 3.12"
+
+from datetime import date
+today = date.today()
+print ('---', today, '---')
+
+work = date(2011, 5, 30)
+delta = today - work
+print("1. work:", int(delta.days/365), '-', int((delta.days%365)/30))
+
+coding = date(2025, 11, 4)
+delta = today - coding
+print("2. coding:", delta.days)
+
+planks = date(2025, 11, 16)
+delta = today - planks
+print("3. planks:", delta.days)
+
+fitness = date(2024, 2, 15)
+delta = today - fitness
+print("4. fitness:", round((delta.days/7), 1))
+
+print("  ", also)

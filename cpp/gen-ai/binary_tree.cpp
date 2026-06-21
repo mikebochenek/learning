a6de8a2cf9945809b284a593e1bfd125c759eb17
Mike Bochenek <mike.bochenek@gmail.com>
2026-06-21 10:25:37 +0200

    slightly better, but does not yet compile https://stackoverflow.com/questions/28914711/getting-error-shared-ptr-in-namespace-std-does-not-name-a-type

--- cpp/gen-ai/binary_tree.cpp
+++ cpp/gen-ai/binary_tree.cpp
@@ -1,11 +1,28 @@
 /** try more cut and paste from web, instead of claude..
     https://bartoszmilewski.com/2013/11/25/functional-data-structures-in-c-trees/ 
 */
+
+#include <iostream>
+#include <memory>
+
 template<class T>
 class Tree {
 public:
     Tree(); // empty tree
     Tree(Tree const & lft, T val, Tree const & rgt);
+};
+
+struct Node
+{
+    Node(std::shared_ptr<const Node> const & lft
+        , T val
+        , std::shared_ptr<const Node> const & rgt)
+    : _lft(lft), _val(val), _rgt(rgt)
+    {}
+
+    std::shared_ptr<const Node> _lft;
+    T _val;
+    std::shared_ptr<const Node> _rgt;
 };
 
 int main() {

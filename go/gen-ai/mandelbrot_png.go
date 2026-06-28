18a68c49124c6702e6f8815f7b7cebb0ff198929
unknown <mike@bochenek.ca>
2026-06-28 09:20:25 +0200

    prompt: in the code\learning\go\gen-ai folder, create a new go file that creates and saves a mandelbrot .png file into a folder c:\temp\ and shows how long the overall process took.  please use only standard libraries part of my golang installation (MAI-Code-1-Flash • 2.6 credits)

---
+++ go/gen-ai/mandelbrot_png.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "fmt"
+    "image"
+    "image/color"
+    "image/png"
+    "math/cmplx"
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    start := time.Now()
+
+    const (
+        width       = 800
+        height      = 600
+        maxIter     = 200
+        scale       = 1.5
+        centerX     = -0.5
+        centerY     = 0.0
+    )
+
+    img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+    for y := 0; y < height; y++ {
+        for x := 0; x < width; x++ {
+            zx := (float64(x-width/2) / float64(width/2)) * scale
+            zy := (float64(y-height/2) / float64(height/2)) * scale
+
+            c := complex(centerX+zx, centerY+zy)
+            z := complex(0, 0)
+            iter := 0
+
+            for iter < maxIter && cmplx.Abs(z) <= 2 {
+                z = z*z + c
+                iter++
+            }
+
+            if iter == maxIter {
+                img.Set(x, y, color.Black)
+            } else {
+                col := color.NRGBA{R: uint8(10 * iter % 256), G: uint8(20 * iter % 256), B: uint8(30 * iter % 256), A: 255}
+                img.Set(x, y, col)
+            }
+        }
+    }
+
+    outputDir := filepath.Join("c:\\", "tmp")
+    if err := os.MkdirAll(outputDir, 0o755); err != nil {
+        panic(err)
+    }
+
+    outputPath := filepath.Join(outputDir, "mandelbrot.png")
+    outFile, err := os.Create(outputPath)
+    if err != nil {
+        panic(err)
+    }
+    defer outFile.Close()
+
+    if err := png.Encode(outFile, img); err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("Saved Mandelbrot image to %s\n", outputPath)
+    fmt.Printf("Total time: %s\n", time.Since(start))
+}

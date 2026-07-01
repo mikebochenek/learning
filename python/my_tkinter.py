6c1bd983e7461edfa6caee02facbe60a47e19e33
unknown <mike@bochenek.ca>
2026-07-01 14:58:15 +0200

    first tinger, but first fix AttributeError: module 'tkinter' has no attribute 'Tk' (consider renaming 'C:\dev\code\learning\python\tkinter.py' since it has the same name as the standard library module named 'tkinter' and prevents importing that standard library module). Did you mean: 'tk'?

---
+++ python/my_tkinter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wednesday July 1 2026
+https://realpython.com/python-gui-tkinter/
+https://tkdocs.com/tutorial/firstexample.html
+
+"""
+import tkinter as tk
+window = tk.Tk()

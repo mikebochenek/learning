25ec8897c08a555d8cf9c31f07469eaf16e78e0a
Mike Bochenek <mike.bochenek@gmail.com>
2026-07-16 09:41:47 +0200

    why is month count starting at zero, and the others not?

--- c/untitled4.c
+++ c/untitled4.c
@@ -21,7 +21,7 @@ int main(void)
    }
 
    printf ("(%ti) ◼ %d-%d-%d ◼ %02d:%02d:%02d\n", 
-      t0, tm->tm_year+1900, tm->tm_mon, tm->tm_mday, 
+      t0, tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, 
       tm->tm_hour, tm->tm_min, tm->tm_sec);
    return 0;
 }

dfbed561c652af0e58ef9c3d2fc89fc8f2519275
Mike Bochenek <mike.bochenek@gmail.com>
2026-07-16 09:41:20 +0200

    print current timestap - not ai gen! but handcoded

--- c/untitled4.c
+++ c/untitled4.c
@@ -1,19 +1,27 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 
 /** https://www.hanshq.net/big-endian-qemu.html
 $ sudo apt-get install qemu-user gcc-mips-linux-gnu
 $ mips-linux-gnu-gcc -static endian.c && qemu-mips a.out
 $ sudo apt-get install gcc-s390x-linux-gnu
 $ s390x-linux-gnu-gcc -static endian.c && qemu-s390x a.out
+and also from - https://stackoverflow.com/a/1445808
+
 */
 int main(void)
 {
    uint32_t x = 0x12345678;
+   time_t t0 = time(0);
+   struct tm *tm =localtime(&t0);
 
    for (int i = 0; i < sizeof(x); i++) {
       printf("\tmem[%d] = 0x%02x\n", i, ((char*)&x)[i]);
    }
 
+   printf ("(%ti) ◼ %d-%d-%d ◼ %02d:%02d:%02d\n", 
+      t0, tm->tm_year+1900, tm->tm_mon, tm->tm_mday, 
+      tm->tm_hour, tm->tm_min, tm->tm_sec);
    return 0;
 }

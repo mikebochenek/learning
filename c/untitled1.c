430b56a91d7b6f649b2fe6e5ed7d5507e731ebfa
Mike Bochenek <mike.bochenek@gmail.com>
2026-07-18 13:51:08 +0200

    minor cleanups, and improve printed logs

--- c/untitled1.c
+++ c/untitled1.c
@@ -5,40 +5,34 @@
  * Mike Bochenek 18.02.2013 21:49:52
  */
 
-struct card
-{
+struct card {
 	char *face;
 	char *suit;
 };
 
-struct deck 
-{
+struct deck  {
 	struct card c[52];
 };
 
-void bsort() 
-{
-	
+void bsort()  {
+	//TODO
 }
 
-void play() 
-{
+void play() {
 	int i;
 	struct card c;
 	for (i = 0; i < 1000/*00000*/; i++) {
-		c.face = malloc(10);
+		c.face = malloc(10); // but wait, that makes no sense!
 	}
 	int x = 10;
-	c.suit = "blah";
+	c.suit = "hearts";
 	printf("welcome to my world of pointers %p", &c);
 	printf("\n%ld %d", sizeof(c.face), x);
-	printf("\n%s", c.suit);
+	printf("\n%s\n", c.suit);
 	free(c.face);
-
 }
 
-int main(void)
-{
+int main(void) {
 	play();
 	bsort();
 	return 0;
